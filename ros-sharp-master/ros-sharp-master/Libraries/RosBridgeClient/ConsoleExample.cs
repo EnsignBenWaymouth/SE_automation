6a205b8af316d6dfe7d37a35dfb7ae63c2e5bf20
benjaminw <benjaminw@PC708>
2021-07-14 14:54:55 +1000

    Added ros-sharp to repo. Initial commit of UR3 simulator code

---
+++ ros-sharp-master/ros-sharp-master/Libraries/RosBridgeClient/ConsoleExample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RosSharp.RosBridgeClient;
+using std_msgs = RosSharp.RosBridgeClient.Messages.Standard;
+
+public class ConsoleExample
+{
+    public static void Main(string[] args)
+    {
+        // WebSocket Connection:
+        RosSocket rosSocket = new RosSocket(new RosBridgeClient.Protocols.WebSocketNetProtocol("ws://172.16.133.1:9090"));
+
+        // Publication:
+        string publication_id = rosSocket.Advertise<std_msgs.String>("publication_test");
+        std_msgs.String message = new std_msgs.String("publication test message data");
+        rosSocket.Publish(publication_id, message);
+
+        // Subscription:
+        string subscription_id = rosSocket.Subscribe<std_msgs.String>("/subscription_test", SubscriptionHandler);
+        subscription_id = rosSocket.Subscribe<std_msgs.String>("/subscription_test", SubscriptionHandler);
+
+        Console.WriteLine("Press any key to close...");
+        Console.ReadKey(true);
+        rosSocket.Close();
+    }
+
+    private static void SubscriptionHandler(std_msgs.String message)
+    {
+        Console.WriteLine((message).data);
+    }
+}
