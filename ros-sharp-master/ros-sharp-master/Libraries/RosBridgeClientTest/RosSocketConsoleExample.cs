11e1fc1f911c621fdb0700db88dfdfc0181dcdf5
benjaminw <benjaminw@PC708>
2021-07-29 14:12:49 +1000

    Removed Textbox popup , replaced with console based selection process

--- ros-sharp-master/ros-sharp-master/Libraries/RosBridgeClientTest/RosSocketConsoleExample.cs
+++ ros-sharp-master/ros-sharp-master/Libraries/RosBridgeClientTest/RosSocketConsoleExample.cs
@@ -165,60 +165,7 @@ namespace RosSharp.RosBridgeClientTest
                     //f = "\\\\elsedge\\engineering\\Drawings\\UR10e\\Modified Orientation\\UR10e.asm";
                     //f = "\\\\elsedge\\engineering\\Drawings\\UR16e\\Modified Orientation\\UR16e.asm";
                     //f = "C:\\Users\\benwa\\Documents\\1 Projects\\Part Library\\ur_robot\\Asm3.asm";
-                }
-
-                //Console.WriteLine("Shown");
-                bool ask_for_user_input = false;
-                if (ask_for_user_input)
-                {
-                    string title = "text box";
-                    string promptText = "File enter name";
-                    string value = "*** Network location ***";
-
-                    Form form = new Form();
-                    Label label = new Label();
-                    TextBox textBox = new TextBox();
-                    Button buttonOk = new Button();
-                    Button buttonCancel = new Button();
-
-                    form.Text = title;
-                    label.Text = promptText;
-                    textBox.Text = value;
-
-                    buttonOk.Text = "OK";
-                    buttonCancel.Text = "Cancel";
-                    buttonOk.DialogResult = DialogResult.OK;
-                    buttonCancel.DialogResult = DialogResult.Cancel;
-
-                    label.SetBounds(9, 20, 372, 13);
-                    textBox.SetBounds(12, 36, 372, 20);
-                    buttonOk.SetBounds(228, 72, 75, 23);
-                    buttonCancel.SetBounds(309, 72, 75, 23);
-
-                    label.AutoSize = true;
-                    textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
-                    buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-                    buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-
-                    form.ClientSize = new Size(396, 107);
-                    form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
-                    form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
-                    form.FormBorderStyle = FormBorderStyle.FixedDialog;
-                    form.StartPosition = FormStartPosition.CenterScreen;
-                    form.MinimizeBox = false;
-                    form.MaximizeBox = false;
-                    form.AcceptButton = buttonOk;
-                    form.CancelButton = buttonCancel;
-
-                    DialogResult dialogResult = form.ShowDialog();
-                    value = textBox.Text;
-
-                    Console.WriteLine(value);
-                    //// 
-                    Console.WriteLine("Value: " + value);
-                    f = value;
-                }
-                
+                }                
 
                 var document_by_name = application.Documents.OpenInBackground<SolidEdgeAssembly.AssemblyDocument>(f);
                 //var document_by_name = application.GetActiveDocument<SolidEdgeAssembly.AssemblyDocument>(false);
